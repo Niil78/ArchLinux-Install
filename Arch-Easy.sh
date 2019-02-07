a3659791e29b43f2d419482ee78b99a348e26ba1
NiilShon <51centimos@gmail.com>
2019-02-07 01:58:29 +0100

    Update and rename Arch-Facil.sh to Arch-Easy.sh
    Finalizado - ended

--- Arch-Easy.sh
+++ Arch-Easy.sh
@@ -75,3 +75,12 @@ arch-chroot /mnt /bin/bash
 tzselect
 pacman -Syu grub efibootmgr iw wpa_supplicant dialog sudo
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ArchLinux
+grub-mkconfig -o /boot/grub/grub.cfg
+echo NStation > /etc/hostname
+useradd -m pepe
+passwd
+echo -e 'configuracion basica minima lista'
+exit
+umount -R /mnt
+swapoff /dev/sda2
+reboot
