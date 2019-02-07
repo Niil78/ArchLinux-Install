6e02c1d5000374f341ee0f87df174fdef6806a4e
NiilShon <51centimos@gmail.com>
2019-02-07 04:05:13 +0100

    Update arch-formato.sh

--- arch-formato.sh
+++ arch-formato.sh
@@ -1,24 +1,24 @@
 echo -e 'Formateando todas las particiones'
 mkfs.fat -F32 /dev/sda1
-echo -e '/boot - formateado'
+echo -e 'Default \e[92mLight green /boot - formateado'
 mkswap /dev/sda2
-echo -e '/swap - formateado'
+echo -e 'Default \e[92mLight green /swap - formateado'
 swapon /dev/sda2
-echo -e '/SWAP ON'
+echo -e 'Default \e[92mLight green /SWAP ON'
 mkfs.ext4 /dev/sda3
-echo -e '/root - formateado'
+echo -e 'Default \e[92mLight green /root - formateado'
 mkfs.ext4 /dev/sda4
-echo -e '/home - formateado'
-echo -e 'Formatos listos'
-echo -e 'montando particiones'
+echo -e 'Default \e[92mLight green /home - formateado'
+echo -e 'Default \e[92mLight green Formatos listos'
+echo -e 'Default \e[92mLight green montando particiones'
 mount /dev/sda3 /mnt
-echo -e '/root - listo'
+echo -e 'Default \e[92mLight green /root - listo'
 mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
-echo -e '/boot - listo'
+echo -e 'Default \e[92mLight green /boot - listo'
 mkdir -p /mnt/home
 mount /dev/sda4 /mnt/home
-echo -e '/home - listo'
+echo -e 'Default \e[92mLight green /home - listo'
 echo -e 'recordamos que SWAP ya fue marcado ON'
 sleep 5
 clear
