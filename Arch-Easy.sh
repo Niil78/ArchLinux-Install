eb940590d2ba15db25a506b817b32f9833f3c806
NiilShon <51centimos@gmail.com>
2019-02-07 03:12:31 +0100

    Update Arch-Easy.sh

--- Arch-Easy.sh
+++ Arch-Easy.sh
@@ -1,14 +1,19 @@
 #################################
 ## ARCH - LINUX BY:NIIL78       #
 #################################
-echo -e 'tienes que estar comnectado a internet.'
+echo -e '{Bold}Bienvenido a la instalacion intectiva'
+echo -e '{Bold}By:Niil78'
+echo -e 'Tienes que estar conectado a internet'
+sleep 1
 echo -e 'vamos a abrir los mirrors y pacman, por si tenemos que hacer cambios'
-sleep 10
+echo -e 'deberias tener un espejo arriba cercano'
+sleep 5
 nano /etc/pacman.conf
 nano /etc/pacman.d/mirrorlist
-echo -e 'Efivars'
+echo -e 'Confirmamos version EFI'
 ls /sys/firmware/efi/efivars
 sleep 5
+clear
 echo -e 'Poniendo la hora'
 timedatectl set-ntp true
 timedatectl status
