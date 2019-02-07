f75fab5e713d2608880321bc928d9efd9e3ec911
NiilShon <51centimos@gmail.com>
2019-02-07 04:05:51 +0100

    Update Arch-Easy.sh

--- Arch-Easy.sh
+++ Arch-Easy.sh
@@ -6,20 +6,20 @@ echo -e '{Bold}By:Niil78'
 echo -e 'Tienes que estar conectado a internet'
 sleep 1
 echo -e 'vamos a abrir los mirrors y pacman, por si tenemos que hacer cambios'
-echo -e 'deberias tener un espejo arriba cercano'
+echo -e 'Default \e[92mLight green deberias tener un espejo arriba cercano'
 sleep 5
 nano /etc/pacman.conf
 nano /etc/pacman.d/mirrorlist
-echo -e 'Confirmamos version EFI'
+echo -e 'Default \e[92mLight green Confirmamos version EFI'
 ls /sys/firmware/efi/efivars
 sleep 5
 clear
-echo -e 'Poniendo la hora'
+echo -e 'Default \e[92mLight green Poniendo la hora'
 timedatectl set-ntp true
 timedatectl status
 sleep 5
 clear
-echo -e 'Listamos los discos duros'
+echo -e 'Default \e[92mLight green Listamos los discos duros'
 lsblk
 echo -e 'ejecutando particiones'
 chmod +x arch-particiones.sh
