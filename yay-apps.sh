84dfc679f38a14b9cc76171b2e90ac8ca02c988a
NiilShon <51centimos@gmail.com>
2019-02-07 18:23:41 +0100

    Update yay-apps.sh

--- yay-apps.sh
+++ yay-apps.sh
@@ -2,4 +2,11 @@
 ## Archivo en construccion
 
 echo -e 'Bienvenido a la instalacion de paquetes asistida'
+echo -e 'Discord'
 yay -Syu && yay -S discord
+echo -e 'Discord instalado'
+echo -e 'Otras'
+yay -S linphone sipp esniper
+echo -e '3D'
+yay -S brlcad freecad 
+
