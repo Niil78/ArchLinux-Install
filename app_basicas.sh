6ac35818982157d7f63bcc19a1e676878bf20b64
NiilShon <51centimos@gmail.com>
2019-02-08 04:42:28 +0000

    Update app_basicas.sh

--- app_basicas.sh
+++ app_basicas.sh
@@ -1,5 +1,5 @@
-# Aqui van algunas aplicaciones que suelo utilizar. 
-# Gestores de paquetes comprimidos 
+# Aqui van algunas aplicaciones que suelo utilizar.
+# Gestores de paquetes comprimidos
 echo -e 'Gestores de paquetes y carptas'
 sudo pacman -Syyu --noconfirm p7zip unrar tar rsync ark steghide thunar
 sleep 4
@@ -11,7 +11,7 @@ sleep 4
 clear
 # Herramientas basicas
 echo -e 'Herramientas basicas'
-sudo pacman -S --noconfirm terminator screen filezilla archey3 ksystemlog libreoffice firefox opera rclone iptraf-ng kdeconnect
+sudo pacman -S --noconfirm terminator screen filezilla archey3 atom ksystemlog libreoffice firefox opera rclone iptraf-ng kdeconnect
 sudo pacman -S --noconfirm weechat teamspeak openvpn inkscape ardour pacmatic arpwatch ntop htop tcpdump
 sudo pacman -S --noconfirm  john nmap tor veracrypt lynis ettercap-gtk hydra nikto sqlmap ifuse aircrack-ng wireshark-gtk teamviewer
 sleep 4
