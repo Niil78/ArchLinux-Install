76b10be141afb042bd63dc55ad096daff5fdae81
NiilShon <51centimos@gmail.com>
2019-02-17 00:29:34 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -30,15 +30,15 @@ while [ opt != '' ]
         case $opt in
         1) clear;
         option_picked "ArchLinux Basico";
-        chmod +x ./Arch-Facil/arch_Easy.sh;
-        chmod +x ./Arch-Facil/arch_basic-install.sh;
+        chmod +x Arch-Facil/arch-Easy.sh;
+        chmod +x Arch-Facil/arch-basic-install.sh;
         sh ./Arch-facil/arch_Easy.sh;
         #show_menu;
         ;;
         2) clear;
         option_picked "ArchLinux Completo";
-        chmod +x .Arch-Facil/arch_Easy.sh;
-        chmod +x .Arch-Facil/arch_full-install.sh;
+        chmod +x Arch-Facil/arch-Easy.sh;
+        chmod +x Arch-Facil/arch-full-install.sh;
         sh .Arch-facil/arch_Easy.sh;
         #show_menu;
         ;;
