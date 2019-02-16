20ddb25498ac40146be289a033958b1d93baa65e
NiilShon <51centimos@gmail.com>
2019-02-17 00:25:40 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -31,7 +31,7 @@ while [ opt != '' ]
         1) clear;
         option_picked "ArchLinux Basico";
         chmod +x /Arch-Facil/arch_Easy.sh;
-        chmod +x /Arch-Facil/arch_basic-install.sh
+        chmod +x /Arch-Facil/arch_basic-install.sh;
         sh /Arch-facil/arch_Easy.sh;
         #show_menu;
         ;;
