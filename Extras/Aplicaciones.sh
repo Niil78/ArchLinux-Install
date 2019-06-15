d0169ffa56eb0f2b6efbde80d08a03df55394f80
NiilShon <51centimos@gmail.com>
2019-06-16 00:55:49 +0200

    fix

--- Extras/Aplicaciones.sh
+++ Extras/Aplicaciones.sh
@@ -1,35 +1,39 @@
 #KDE PLASMA
+echo -e 'KDE&SDDM'
 pacman -Syu --noconfirm plasma plasma-meta sddm
 systemctl enable sddm
 #systemctl restart sddm
 #Descomentar si se usa tras la instalacion y no durante.
 ##
 ##BASICAS
+echo -e 'Aplicaciones NECESARIAS'
 pacman -S iw wpa_supplicant dialog vim sudo
 ##
 # Aqui van algunas aplicaciones que suelo utilizar.
 # Gestores de paquetes comprimidos
 echo -e 'Gestores de paquetes y carptas'
 sudo pacman -Syyu --noconfirm p7zip unrar tar rsync ark  thunar
-sleep 4
 clear
 # Arduino y otras herramientas
 echo -e 'Arduino y otras herramientas'
-sudo pacman -S --noconfirm notepadqq mtpfs kio-mtp atom arduino arduino-builder arduino-docs arduino-cli arduino-ctags arduino-avr-core
-sleep 4
+sudo pacman -S --noconfirm notepadqq mtpfs kio-mtp atom arduino arduino-builder arduino-docs arduino-cli arduino-ctags arduino-avr-core atom openvpn
 echo -e 'Bluetooth y otras'
 sudo pacman -S --noconfirm bluez bluedevil bluez-utils pulseaudio-bluetooth
 clear
+#Seguridad informatica & otr@s
+echo -e 'Seguridad informatica & otras'
+sudo pacman -S --noconfirm rclone aircrack-ng wireshark-gtk veracrypt john nmap tor
+sudo pacman -S --noconfirm arpwatch ettercap-gtk hydra nikto lynis sqlmap iptraf-ng
 # Herramientas basicas
 echo -e 'Herramientas basicas'
-sudo pacman -S --noconfirm terminator screen filezilla atom ksystemlog libreoffice firefox opera rclone iptraf-ng kdeconnect
-sudo pacman -S --noconfirm weechat teamspeak openvpn inkscape ardour pacmatic arpwatch ntop htop tcpdump steghide
-sudo pacman -S --noconfirm john nmap tor veracrypt lynis ettercap-gtk hydra nikto sqlmap ifuse aircrack-ng wireshark-gtk teamviewer
+sudo pacman -S --noconfirm terminator screen filezilla  ksystemlog libreoffice firefox opera
+sudo pacman -S --noconfirm weechat teamspeak inkscape ntop htop tcpdump steghide
+sudo pacman -S --noconfirm kdeconnect ifuse teamviewer
 sudo pacman -S --noconfirm kdesdk-thumbnailers kdf sweeper cbatticon
-
-sleep 4
+sudo pacman -S --noconfirm ardour pacmatic
 clear
 # 3D printer
+echo -e 'SoftWare 3D Printers'
 sudo pacman -S --noconfirm cura curaengine cura-binary-data
 sleep 4
 clear
--- readme.md
+++ readme.md
@@ -10,11 +10,9 @@ Uso libre para todo el mundo.
 | InstalacionNueva           | Instalacion Completa nueva.           | 1.5Horas |
 | Extras                     | Instalación Gestores&Herramientas     | 1/2Horas |
 
+<h1>MirrorsList</h1>
+Los mirrors han sido modificados.
+ESPAÑA,FRANCIA&Portgual Son los mirrors que hay ahora en la instalacion.
 
 <h1>Como usarlo:</h1>
 git clone https://github.com/niil78/ArchLinux-install && chmod +x menu.sh && sh menu.sh
-
-La instalación completa tarda aproximadamente 2H en terminar.
-
-
-La instalación básica tarda aproximadamente 15 minutos en terminar.
