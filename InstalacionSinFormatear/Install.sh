e52f604ff0e20c80e83bdfc8fd23da28bda43534
NiilShon <51centimos@gmail.com>
2019-06-15 09:40:39 +0200

    Update Install.sh

--- InstalacionSinFormatear/Install.sh
+++ InstalacionSinFormatear/Install.sh
@@ -64,41 +64,41 @@ genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
 arch-chroot /mnt /bin/bash
 
-#################################################
-## Configuracion del S.O Basico
-################################################
-nano /etc/locale.gen
-locale-gen
-echo -e 'LANG=en_US.UTF-8'
-sleep 3
-tzselect #Para configurar el Relog
-pacman -Syu grub efibootmgr iw wpa_supplicant dialog sudo
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ArchLinux
-grub-mkconfig -o /boot/grub/grub.cfg
-## Instalacion del GRUB & configuracion BASE.
-echo NStation > /etc/hostname
-##Marcamos la maquina como NStation(PuedeCambiarse)
-### Creacion de usuario SIN PERMISOS ROOT.
-echo -e '\e[91mCreando un Usuario\e[0m'
-echo -e '\e[92mPor defecto voy a crear a Niil\e[0m'
-useradd -m niil
-passwd niil
-echo -e '\e[91mContraseña root\e[0m'
-passwd
-echo -e '\e[91mConfiguracion basica minima lista\e[0m'
-#KDE - Plasma
-pacman -Syu --noconfirm plasma plasma-meta sddm
-systemctl enable sddm
-#Desmontar y preparar para finalizar
-exit #salimos de la particion DONDE Instalamos el SO
-echo -e '\e[92mDesmontando todas las particiones y reinciando\e[0m'
-sleep 1
-lsblk
-umount -R /mnt
-lsblk
-swapoff /dev/sda2
-echo -e '\e[92mel swap tambien\e[0m'
-chmod +x Fin.sh
-sh Fin.sh
-sleep 1
-reboot
+# #################################################
+# ## Configuracion del S.O Basico
+# ################################################
+# nano /etc/locale.gen
+# locale-gen
+# echo -e 'LANG=en_US.UTF-8'
+# sleep 3
+# tzselect #Para configurar el Relog
+# pacman -Syu grub efibootmgr iw wpa_supplicant dialog sudo
+# grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ArchLinux
+# grub-mkconfig -o /boot/grub/grub.cfg
+# ## Instalacion del GRUB & configuracion BASE.
+# echo NStation > /etc/hostname
+# ##Marcamos la maquina como NStation(PuedeCambiarse)
+# ### Creacion de usuario SIN PERMISOS ROOT.
+# echo -e '\e[91mCreando un Usuario\e[0m'
+# echo -e '\e[92mPor defecto voy a crear a Niil\e[0m'
+# useradd -m niil
+# passwd niil
+# echo -e '\e[91mContraseña root\e[0m'
+# passwd
+# echo -e '\e[91mConfiguracion basica minima lista\e[0m'
+# #KDE - Plasma
+# pacman -Syu --noconfirm plasma plasma-meta sddm
+# systemctl enable sddm
+# #Desmontar y preparar para finalizar
+# exit #salimos de la particion DONDE Instalamos el SO
+# echo -e '\e[92mDesmontando todas las particiones y reinciando\e[0m'
+# sleep 1
+# lsblk
+# umount -R /mnt
+# lsblk
+# swapoff /dev/sda2
+# echo -e '\e[92mel swap tambien\e[0m'
+# chmod +x Fin.sh
+# sh Fin.sh
+# sleep 1
+# reboot
