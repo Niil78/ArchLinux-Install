ff43106b642c395869919154260604c308b459c1
NiilShon <51centimos@gmail.com>
2022-09-23 20:55:03 +0200

    Update Install.sh

--- InstalacionSinFormatear/Install.sh
+++ InstalacionSinFormatear/Install.sh
@@ -96,7 +96,7 @@ echo -e '\e[92mrecordamos que SWAP ya fue marcado ON\e[0m'
 echo -e '\e[92mSi este programa se para tras unos instantes y apareces logeado como root\e[0m'
 echo -e '\e[92mEntonces tu instalacion esta apunto de terminar. unicamente deberas ejecutar install-2.sh\e[0m'
 sleep 2
-pacstrap /mnt base linux linux-firmware
+pacstrap /mnt base linux linux-firmware zsh
 sleep 1
 genfstab -U /mnt >> /mnt/etc/fstab
 sleep 1
